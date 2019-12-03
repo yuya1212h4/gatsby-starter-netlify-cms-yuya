44ea45fb2bde6d5c23cf03f00c54be4cd37e69e0
yuya1212h4 <yuya1212h4.066@gmail.com>
2019-12-03 15:46:54 +0900

    create news page admin function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -75,6 +75,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link>
+              <Link className="navbar-item" to="/news">
+                news
+              </Link>
             </div>
             <div className="navbar-end has-text-centered">
               <a
